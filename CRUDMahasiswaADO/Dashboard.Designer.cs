5cfa362fd07f41402b6e9a7b15c460af06ca9108
Fasya Tri.N <fasyanovember@gmail.com>
2026-06-18 19:09:35 +0700

    mengubah button load

--- CRUDMahasiswaADO/Dashboard.Designer.cs
+++ CRUDMahasiswaADO/Dashboard.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.dtpTanggalMasuk = new System.Windows.Forms.DateTimePicker();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -50,14 +50,15 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Tahun Masuk";
             // 
-            // button1
+            // btnLoad
             // 
-            this.button1.Location = new System.Drawing.Point(334, 43);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnLoad.BackColor = System.Drawing.Color.GreenYellow;
+            this.btnLoad.Location = new System.Drawing.Point(334, 43);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 2;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
@@ -74,7 +75,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnLoad);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dtpTanggalMasuk);
             this.Name = "Dashboard";
@@ -88,7 +89,7 @@
 
         private System.Windows.Forms.DateTimePicker dtpTanggalMasuk;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnLoad;
         private System.Windows.Forms.Button button2;
     }
 }
